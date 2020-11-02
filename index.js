107fb470bfbf56ec437574584429431b4a6ce8d1
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-11-02 19:56:41 -0300

    ejercicio 2 propuesto resuelto

--- index.js
+++ index.js
@@ -3,19 +3,40 @@
 
 // obtenerNumeroMayor([2, 7, 12, 1]) // 12
 
-const numeros = [2, 7, 12, 1]
+// const numeros = [2, 7, 12, 1]
 
-const obtenerNumeroMayor = (numeros)=>{
- let numeroMasGrande = 0
- for (let i = 0; i < numeros.length; i++) {
+// const obtenerNumeroMayor = (numeros)=>{
+//  let numeroMasGrande = 0
+//  for (let i = 0; i < numeros.length; i++) {
    
-    if(numeros[i]>numeroMasGrande){
-        numeroMasGrande = numeros[i];
-    }
- }
+//     if(numeros[i]>numeroMasGrande){
+//         numeroMasGrande = numeros[i];
+//     }
+//  }
  
- return numeroMasGrande
+//  return numeroMasGrande
+
+// }
+
+// console.log(obtenerNumeroMayor(numeros))
+
+// Crear una función sumarArray que tome como argumento un array de números
+// y devuelva la suma de todos los número de dicho array.
+
+// sumar([4, 5, 10]) // 19
+
+const arrayDeNumeros = [4 , 5, 10]
+
+const sumarArray=(arrayDeNumeros)=>{
+    let acumuladora = 0
+    for (let i = 0; i < arrayDeNumeros.length; i++) {
+        
+        acumuladora += arrayDeNumeros[i]
+        
+    }
+
+    return acumuladora
 
 }
 
-console.log(obtenerNumeroMayor(numeros))
+console.log(sumarArray(arrayDeNumeros))
